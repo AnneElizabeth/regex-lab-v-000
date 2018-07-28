25dcf0b16e6726ce43a3cfdc590ecd35300774fa
Anne Edmondson <edmondson.anne@gmail.com>
2018-07-28 19:52:16 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,6 @@
 def starts_with_a_vowel?(word)
-  if word.scan(/\b[AEIOUaeiou]*\b/)
-    return true 
-  else
-    return false
-  end
+   word.scan(/\b[AEIOUaeiou]*\b/)
+    
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
