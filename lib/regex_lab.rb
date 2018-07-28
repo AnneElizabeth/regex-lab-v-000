9e0e5f005550c6446368eb8047256227cda0aa60
Anne Edmondson <edmondson.anne@gmail.com>
2018-07-28 18:41:30 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,9 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-   if word.scan(\b)
+   if word.scan(\b[Uu]n[a-z]*ing\b)
+     true
+   end
 end
 
 def words_five_letters_long(text)
