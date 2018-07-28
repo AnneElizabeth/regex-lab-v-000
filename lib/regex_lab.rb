522a7c0c49453cf73c3f5715d3cb8507a34e63e0
Anne Edmondson <edmondson.anne@gmail.com>
2018-07-28 18:42:23 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -5,7 +5,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-   if text.scan(\b[Uu]n[a-z]*ing\b)
+   if text.scan(/\b[Uu]n[a-z]*ing\b/)
      true
    end
 end
